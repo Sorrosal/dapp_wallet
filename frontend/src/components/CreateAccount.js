b48c335a55c17f166ee2c3cff260305fd90c6e0d
Sergio Sorrosal <sergiosorrosalgayan@gmail.com>
2024-07-07 19:37:12 +0200

    Create account

--- frontend/src/components/CreateAccount.js
+++ frontend/src/components/CreateAccount.js
@@ -1,7 +1,56 @@
 import React from 'react'
+import { Button, Card } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { ethers } from "ethers";
 
-export const CreateAccount = () => {
+export const CreateAccount = ({ setWallet, setSeedPhrase }) => {
+  const [newSeedPhrase, setNewSeedPhrase] = useState(null);
+  const navigate = useNavigate();
+
+  function generateWallet() {
+    const mnemonic = ethers.Wallet.createRandom().mnemonic.phrase;
+    setNewSeedPhrase(mnemonic);
+  }
+
+  function setWalletAndMnemonic() {
+    setNewSeedPhrase(newSeedPhrase);
+    setWallet(ethers.Wallet.fromPhrase(newSeedPhrase).address);
+  }
   return (
-    <div>CreateAccount</div>
+    <>
+    <div className='content'>
+      <div className='mnemonic'>
+        <ExclamationCircleOutlined style={{fontSize: "20px"}} />
+        <div>
+        Once you generate the seed phrase, store it securely so you can retrieve your wallet in the
+        your wallet in the future.
+        </div>
+      </div>
+      <Button
+        className='frontPageButton'
+        type='primary'
+        onClick={() => generateWallet()}
+      >
+        Generate seed phrase
+      </Button>
+      <Card className='seedPhraseContainer'>
+        {newSeedPhrase && (
+          <pre style={{whiteSpace:"pre-wrap"}}>{newSeedPhrase}</pre>
+        )}
+      </Card>
+      <Button
+        className='frontPageButton'
+        type='default'
+        onClick={() => setWalletAndMnemonic()}
+      >
+        Create your new wallet
+      </Button>
+      <p className='frontPageBottom' onClick={() => navigate("/")}>
+        Back
+      </p>
+    </div>
+    </>
   )
 }
